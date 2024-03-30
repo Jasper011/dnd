65bc731ebd3d431b2816b3af49c8cff5daa233a2
Ilya <splint1777@gmail.com>
2024-03-30 14:01:16 +0300

    fix item inserting into the cage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const cages = document.querySelectorAll('#cages>.cage');
+const cages = document.querySelector('#cages');
 const items = Array.from(document.querySelectorAll('.item'));
 const addBtn = document.querySelector('#addBtn')
 const removeBtn = document.querySelector('#removeBtn')
@@ -6,8 +6,8 @@ const removeInput = document.querySelector('#removeInput')
 let maxId = 0;
 
 addBtn.addEventListener('click', addItem)
-removeBtn.addEventListener('click', (event)=>{
-    try{
+removeBtn.addEventListener('click', (event) => {
+    try {
         deleteItem(removeInput.value)
     } catch {
         event.preventDefault()
@@ -15,7 +15,7 @@ removeBtn.addEventListener('click', (event)=>{
 })
 
 function addItem() {
-    for (let cage of cages) {
+    for (let cage of cages.querySelectorAll('.cage')) {
         if (cage.children.length == 0) {
             const newItem = document.createElement('div');
             newItem.classList.add('item');
@@ -36,7 +36,7 @@ function initItems() {
             item.id = maxId;
             maxId++;
         }
-        item.addEventListener('mousedown', (event)=>{
+        item.addEventListener('mousedown', (event) => {
             let shiftX = event.clientX - item.getBoundingClientRect().left;
             let shiftY = event.clientY - item.getBoundingClientRect().top;
             item.style.position = 'absolute'
@@ -45,14 +45,20 @@ function initItems() {
                 item.style.top = y - shiftY + 'px'
                 item.style.left = x - shiftX + 'px'
             }
-            function onMove(event){
+            function onMove(event) {
                 moveItem(event.pageX, event.pageY)
             }
             document.addEventListener('mousemove', onMove)
-            item.addEventListener('mouseup', function onUp(event){
+            item.addEventListener('mouseup', function onUp(event) {
+                const coords = cages.getBoundingClientRect();
+                const cageNum = ((Math.floor((event.clientY - coords.y) / 100)) * 5 + Math.floor((event.clientX - coords.x) / 100))
+                console.log(cageNum);
+                const cageHTML = cages.querySelectorAll('.cage')[cageNum]
+                console.log(cageHTML);
+                cageHTML.append(event.currentTarget)
+                event.currentTarget.style.position = 'static'
                 document.removeEventListener('mousemove', onMove)
                 item.removeEventListener('mouseup', onUp)
-                console.log(event.target);
             })
         })
     }
@@ -62,11 +68,4 @@ function deleteItem(id) {
     items.forEach(item => { if (item.id == id) item.remove() })
 }
 
-function initCages() {
-    for (let cage of cages) {
-        
-    }
-}
-
-initCages()
 initItems()
